3be3990723946a6e496b337a3a0df156521a07dd
SUBZERO-MD <davidpatrf@gmail.com>
2026-04-16 07:08:49 -0500

    Update OWNER_NUMBER format in settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   SESSION_ID: "Ice~PtwHhJ", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "51907071260", // Put Owner Number Here 
+  OWNER_NUMBER: "+51907071260", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
